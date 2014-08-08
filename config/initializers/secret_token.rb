f479c21fa39fbe3ce2bba6270643c232d410591c
Edgar <liedgar7@gmail.com>
2014-08-07 22:53:37 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VitSite::Application.config.secret_key_base = '18983a99aa43b99e7160890a63e379c2960a2eb2a47f59f8efcdd132d34f7468078a19d6f927e849627c08c451755e1e7bdfa03026d5b5843763ad745a878294'
