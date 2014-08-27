fe7d5ccbb7969847a5b1a83076b892e7363d22bd
Edgar <liedgar7@gmail.com>
2014-08-27 13:27:12 -0400

    finish css

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 VitSite::Application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp
+
+ActionMailer::Base.server_settings = {
+   :address => "smtp.vit.fitness",
+   :port => 25,
+   :domain => "vit.fitness",
+   :authentication => :login,
+   :user_name => "username",
+   :password => "password",
+}
+
+ActionMailer::Base.default_content_type = "text/html"
